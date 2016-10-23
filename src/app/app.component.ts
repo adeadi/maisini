94ce7a0b54880440fe6258efc5b13fb380752cba
Ade Adi Wibowo <Ade Adi Wibowo>
2016-10-23 23:11:28 +0800

    add google map marker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,31 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import {
+    Component
+} from '@angular/core';
+import {
+    Platform
+} from 'ionic-angular';
+import {
+    StatusBar,
+    Splashscreen
+} from 'ionic-native';
 
-import { HomePage } from '../pages/home/home';
+import {
+    HomePage
+} from '../pages/home/home';
 
 
 @Component({
-  templateUrl: '../pages/app.html'
+    templateUrl: '../pages/app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+    rootPage = HomePage;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-    });
-  }
+    constructor(platform: Platform) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+            Splashscreen.hide();
+        });
+    }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,50 +1,88 @@
-import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
-import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
+import {
+    Component
+} from '@angular/core';
+import {
+    NavController,
+    Platform
+} from 'ionic-angular';
+import {
+    GoogleMap,
+    GoogleMapsEvent,
+    GoogleMapsLatLng,
+    GoogleMapsMarkerOptions,
+    GoogleMapsMarker,
+    Geolocation
+} from 'ionic-native';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
     map: GoogleMap;
 
-   constructor(public navCtrl: NavController, public platform: Platform) {
+    constructor(public navCtrl: NavController, public platform: Platform) {
         platform.ready().then(() => {
             this.loadMap();
         });
     }
 
-    loadMap(){
+    loadMap() {
+        Geolocation.getCurrentPosition().then((position) => {
+            let location = new GoogleMapsLatLng(position.coords.latitude, position.coords.longitude);
 
-        let location = new GoogleMapsLatLng(-34.9290,138.6010);
+            this.map = new GoogleMap('map', {
+                'backgroundColor': 'white',
+                'controls': {
+                    'compass': true,
+                    'myLocationButton': true,
+                    'indoorPicker': true,
+                    'zoom': true
+                },
+                'gestures': {
+                    'scroll': true,
+                    'tilt': true,
+                    'rotate': true,
+                    'zoom': true
+                },
+                'camera': {
+                    'latLng': location,
+                    'tilt': 30,
+                    'zoom': 15,
+                    'bearing': 50
+                }
+            });
 
-        this.map = new GoogleMap('map', {
-          'backgroundColor': 'white',
-          'controls': {
-            'compass': true,
-            'myLocationButton': true,
-            'indoorPicker': true,
-            'zoom': true
-          },
-          'gestures': {
-            'scroll': true,
-            'tilt': true,
-            'rotate': true,
-            'zoom': true
-          },
-          'camera': {
-            'latLng': location,
-            'tilt': 30,
-            'zoom': 15,
-            'bearing': 50
-          }
+            this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+                let markerOptions: GoogleMapsMarkerOptions = {
+                    position: location,
+                    title: 'Ionic Title\n is Awesome!',
+                    snippet: 'This is snippet',
+                    styles: {
+                        'text-align': 'center',
+                        'font-style': 'italic',
+                        'font-weight': 'bold',
+                        'color': 'red'
+                    },
+                    icon: 'blue'
+                };
+                this.map.addMarker(markerOptions).then(
+                    (marker: GoogleMapsMarker) => {
+                        marker.showInfoWindow();
+                        marker.addEventListener(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+                            alert("marker is clicked");
+                        });
+                        marker.addEventListener(GoogleMapsEvent.INFO_CLICK).subscribe(() => {
+                            alert("InfoWindow is clicked");
+                        });
+                    }
+                );
+
+                console.log('Map is ready!');
+            });
+
+        }, (err) => {
+            console.log(err);
         });
-
-        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
-            console.log('Map is ready!');
-        });
-
     }
-
 }
